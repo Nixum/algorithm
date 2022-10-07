0271d9de25fa0ba203d3b007a5ae46fc31cfeb81
nixum <731604348@qq.com>
2022-10-07 21:12:22 +0800

    add greedy algorithm

---
+++ greedy/can_complete_circuit_134.go
@@ -0,0 +1,19 @@
+package greedy
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	start := 0
+	curSum := 0
+	totalSum := 0
+	for i := 0; i < len(gas); i++ {
+		curSum += gas[i] - cost[i]
+		totalSum += gas[i] - cost[i]
+		if curSum < 0 {
+			start = i + 1
+			curSum = 0
+		}
+	}
+	if totalSum < 0 {
+		return -1
+	}
+	return start
+}
