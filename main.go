c44777b6e2e9c96e04ae65198409ff1df8573bf1
nixum <731604348@qq.com>
2023-03-10 02:03:34 +0800

    remove extra code

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import "algorithm/binarytree"
-
 func main() {
 
-}
-
-func lowestCommonAncestor(root, p, q *binarytree.TreeNode) *binarytree.TreeNode {
-	if root == nil || root == p || root == q {
-		return root
-	}
-	left := lowestCommonAncestor(root.Left, p, q)
-	right:= lowestCommonAncestor(root.Right, p, q)
-	if left != nil && right != nil {
-		return root
-	} else if left == nil && right != nil {
-		return right
-	} else if left != nil && right == nil {
-		return left
-	}
-	return nil
 }
